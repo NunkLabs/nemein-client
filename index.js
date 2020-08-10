a3f0a66a9b2ecba9c985fa7a4146b21c1b6fbd95
Junwei Chen <jwchen1259@gmail.com>
2020-08-10 12:23:45 -0700

    chore: setup MongoDB/Mongoose

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -7,6 +9,11 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
