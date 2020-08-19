ac77b6e80537a2bda95eed601db85dad1015127d
Junwei Chen <jwchen1259@gmail.com>
2020-08-18 22:03:32 -0700

    fix(client:LoginForm): Associate labels with inputs

--- src/client/src/components/LoginForm/LoginForm.tsx
+++ src/client/src/components/LoginForm/LoginForm.tsx
@@ -6,15 +6,19 @@ const LoginMenu = (): JSX.Element => (
   <Menu isOpen outerContainerId="outer-container" pageWrapId="game-wrap" width="600px">
     <div className="form">
       <form>
-        <div className='mb-2'>
-          <label className="form-label">Username</label>
-          <input type="email" className="form-control-custom" />
+        <div className="mb-2">
+          <label className="form-label" htmlFor="username-input">
+            Username
+            <input type="email" className="form-control-custom" id="username-input" />
+          </label>
         </div>
-        <div className='mb-2'>
-          <label className="form-label">Password</label>
-          <input type="password" className="form-control-custom"/>
+        <div className="mb-2">
+          <label className="form-label" htmlFor="password-input">
+            Password
+            <input type="password" className="form-control-custom" id="password-input" />
+          </label>
         </div>
-        <div className='mb-2'>
+        <div className="mb-2">
           <button type="submit" className="btn-custom btn-custom-dark btn-block">Log In</button>
           <button type="submit" className="btn-custom btn-custom-dark btn-block">Register Now</button>
         </div>
