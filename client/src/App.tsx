48eadd2beee73c8ac014e1df0fab0077c47fd2a1
Junwei Chen <jwchen1259@gmail.com>
2020-09-06 09:02:38 -0700

    refactor(client): Use CDN for Bootstrap instead

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import Home from './pages/Home/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = (): JSX.Element => (
   <Router>
