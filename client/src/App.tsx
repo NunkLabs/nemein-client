c6e2d959550c92c020d5f1d5c1fc6e793ef7e8d8
Junwei Chen <jwchen1259@gmail.com>
2020-09-06 08:35:13 -0700

    fix(client): Use react-router-dom's Switch component again

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import Home from './pages/Home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = (): JSX.Element => (
-  <BrowserRouter>
+  <Router>
     <Switch>
-      <Route path="/" component={Landing} />
-      <Route path="/home" component={Home} />
+      <Route exact path="/">
+        <Landing />
+      </Route>
+      <Route exact path="/home">
+        <Home />
+      </Route>
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
