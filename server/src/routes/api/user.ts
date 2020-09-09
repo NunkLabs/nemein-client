3ae3f068d174edb459252340415492692e016951
Junwei Chen <jwchen1259@gmail.com>
2020-09-08 17:46:03 -0700

    feat(server:API): Add some more basic User endpoints

--- server/src/routes/api/user.ts
+++ server/src/routes/api/user.ts
@@ -6,8 +6,8 @@ import logger from '../../utils/logger';
 
 const router = express.Router();
 
-/* GET: user info */
-router.get('/info', async (req, res) => {
+/* GET: user's full info */
+router.get('/', async (req, res) => {
   if (req.isAuthenticated()) {
     try {
       if (req.session) {
@@ -20,6 +20,33 @@ router.get('/info', async (req, res) => {
             displayName: dbUser.displayName,
             profilePic: dbUser.profilePic,
             createdAt: dbUser.createdAt,
+          });
+        }
+      }
+
+      return res.status(404).end();
+    } catch (err) {
+      logger.error(err);
+
+      return res.status(500).end();
+    }
+  } else {
+    res.redirect('/landing');
+
+    return res.status(401).end();
+  }
+});
+
+/* GET: user's top 5 personal score in descending order */
+router.get('/scores', async (req, res) => {
+  if (req.isAuthenticated()) {
+    try {
+      if (req.session) {
+        const { user } = req.session.passport;
+
+        const dbUser = await User.findOne({ _id: user });
+        if (dbUser) {
+          return res.send({
             scores: dbUser.scores,
           });
         }
@@ -38,8 +65,42 @@ router.get('/info', async (req, res) => {
   }
 });
 
-/* POST: update scores */
-router.put('/update_scores', async (req, res) => {
+/* PUT: update user's info */
+router.put('/update', async (req, res) => {
+  if (req.isAuthenticated()) {
+    try {
+      if (req.session) {
+        const { user } = req.session.passport;
+        const dbUser = await User.findOne({ _id: user });
+
+        if (dbUser) {
+          const { newEmail, newName, newProfilePic } = req.body;
+
+          await User.findOneAndUpdate({ _id: user }, {
+            emailAddress: newEmail || dbUser.emailAddress,
+            displayName: newName || dbUser.displayName,
+            profilePic: newProfilePic || dbUser.profilePic,
+          });
+
+          return res.status(200).end();
+        }
+      }
+
+      return res.status(404).end();
+    } catch (err) {
+      logger.error(err);
+
+      return res.status(500).end();
+    }
+  } else {
+    res.redirect('/landing');
+
+    return res.status(401).end();
+  }
+});
+
+/* PUT: update user's scores */
+router.put('/update/scores', async (req, res) => {
   if (req.isAuthenticated()) {
     try {
       if (req.session) {
