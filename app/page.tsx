8eb406233b77e365b04d9636cbf05c8c07237f8c
Junwei Chen <jwchen1259@gmail.com>
2023-05-27 13:15:44 +0700

    refactor: rename variables for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import dynamic from "next/dynamic";
 
-const MyStage = dynamic(() => import("./(game)/Game"), { ssr: false });
+const GameStage = dynamic(() => import("./(game)/Game"), { ssr: false });
 
 export default function HomePage() {
   return (
     <div className="bg-gray-800 h-screen min-w-fit relative">
-      <MyStage />
+      <GameStage />
     </div>
   );
 }
