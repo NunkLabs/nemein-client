38dfd1ff0acd7f693aee04a4142238e95c8f4445
Junwei Chen <jwchen1259@gmail.com>
2023-05-29 16:53:58 +0700

    chore: add security headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,49 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  connect-src 'self' ws: tetribass.com *.tetribass.com ;
+  img-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval'
+`;
+
 module.exports = {
   compress: false,
-  experimental: {
-    appDir: true,
-  },
+  poweredByHeader: false,
   reactStrictMode: process.env.NODE_ENV === "production",
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), geolocation=(), microphone=()",
+          },
+          {
+            key: "Server",
+            value: "tetribass",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
 };
