59fa9a772933ad4bcff5aa2cf14e30bc8b31824d
Junwei Chen <jwchen1259@gmail.com>
2023-06-06 15:57:47 +0700

    chore: enable Tailwind class sorting & correct lint-stage order

--- app/(game)/(prompts)/Control.tsx
+++ app/(game)/(prompts)/Control.tsx
@@ -8,7 +8,7 @@ type ControlProps = {
 export default function ControlPrompt({ isOver, restartGame }: ControlProps) {
   return (
     <div className={"control"}>
-      <p className={"font-bold text-slate-100 text-3xl"}>
+      <p className={"text-3xl font-bold text-slate-100"}>
         {isOver ? "Game Over" : "Settings"}
       </p>
       <button className={`button button-light h-8 w-40`} onClick={restartGame}>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import GameStage from "./(game)/GameStage";
 
 export default function HomePage() {
   return (
-    <div className="bg-gray-800 h-screen min-w-fit relative">
+    <div className="relative h-screen min-w-fit bg-gray-800">
       <GameStage />
     </div>
   );
